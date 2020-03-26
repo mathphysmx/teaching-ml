0295f657809c9e7b866ff9014470cae06f7723e9
Francisco Mendoza-Torres <mentofran@gmail.com>
2020-03-26 09:57:00 -0600

    update

---
+++ evaluation/Exercises/francisco_clustering_silhouette.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = pd.DataFrame({'x': [1,1,2,3,4,5], 'y':[4,6,6,8,3,2]})
+
+i = 0
+x.iloc[i, ]
+
+
+np.sqrt(3)
+def dist(x1,x2):
+    return((x1.x - x2.x)^2)
