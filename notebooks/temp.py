bec615902d665a11cfc3b62d5be374ca9f7846a8
Francisco Mendoza-Torres <mentofran@gmail.com>
2020-05-12 03:10:54 -0500

    Decission trees

--- notebooks/temp.py
+++ notebooks/temp.py
@@ -1,3 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+iris = load_iris()
+X = iris.data[:, 2:] # petal length and width
+y = iris.target
+tree_clf = DecisionTreeClassifier(max_depth=2)
+tree_clf.fit(X, y)
+tree_clf.predict_proba([[5, 1.5]])
+tree_clf.predict([[5, 1.5]])
+
+################33
+x = np.linspace(0,1, 21)
+y=x*(1-x)
+plt.plot(x, y, 'b-')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.show()
+
+
+#############
+
+
+################
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+from winsound import MessageBeep
+MessageBeep()
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+plt.imshow(train_images[0])
+plt.show()
+
+train_images = train_images/255
+test_images = test_images/255
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+model=keras.Sequential([
+  keras.layers.Flatten(input_shape=(28,28)),
+  keras.layers.Dense(128, activation='relu'),
+  keras.layers.Dense(256, activation='relu'),
+  keras.layers.Dense(10)
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10,validation_split=0.25)
+
+a=model.evaluate(test_images, test_labels)
+
+
+i=9990
+res=model.predict(np.expand_dims(test_images[i],0))
+print('prediccion: ', class_names[np.argmax(res)])
+print('etiqueta:   ', class_names[test_labels[i]])
+plt.imshow(test_images[i])
+plt.show()
+
+probability_model=keras.Sequential([
+  model,
+  keras.layers.Softmax()
+])
+
+probability_model.predict(np.expand_dims(test_images[i],0))
 
 
 
@@ -9,6 +88,56 @@
 
 
 
+a = model.evaluate(test_images, test_labels)
+print('accuracy: ', a[1])
+
+
+b= model.predict(test_images)
+b[0:3
+]
+
+
+probability_model=keras.Sequential([
+  model,
+  keras.layers.Softmax()
+])
+
+predictions = probability_model.predict(test_images)
+
+probability_model.predict(np.expand_dims(test_images[4],0))
+
+
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 
 
@@ -16,10 +145,13 @@
 
 
 
-
-
-
-
+i = 0
+plt.figure(figsize=(2*6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
 
 
 ##################################
